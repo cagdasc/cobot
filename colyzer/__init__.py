ab1b495b6dd1ddb1b7b354210e349b26332b9aeb
cagdas <cagdascaglak@gmail.com>
2015-04-25 20:14:34 +0300

    Shingle Based implemented

--- colyzer/__init__.py
+++ colyzer/__init__.py
@@ -2,3 +2,11 @@ __author__ = 'cagdascaglak'
 import os
 
 SITES = os.path.join(os.getcwd(), '..', 'sites')
+
+tag_weight= {'a':0.8, 'abbr': 0.08, 'body': 0.8,'audio': 0.1, 'b': 0.38, 'br': 0.28, 'button': 0.47, 'code': 0.33,
+             'col': 0.157, 'dd': 0.6, 'form': 0.78, 'head': 0.8, 'img': 0.7, 'h1': 0.7, 'h2': 0.7, 'h3': 0.7, 'h4': 0.7,
+             'h5': 0.7, 'h6': 0.7, 'header': 0.66, 'html': 1, 'iframe': 0.32, 'input': 0.69, 'li': 0.77, 'link': 0.88,
+             'main': 0.4, 'mark': 0.4, 'menuitem': 0.4, ' meter': 0.4, 'nav': 0.6, 'ol': 0.77, 'output': 0.4, 'p': 0.79,
+             'script': 0.8, 'section': 0.7, 'strong': 0.43, 'style': 0.55, 'table': 0.87, 'td':0.87, 'th': 0.87, 'tr': 0.87,
+             'title': 0.6, 'ul':0.78, 'div': 0.86, 'span': 0.4, 'dl': 0.3, 'hr': 0.3, 'dt': 0.3, 'tt': 0.2, 'em': 0.2,
+             'footer': 0.3, 'i': 0.3}
--- colyzer/main.py
+++ colyzer/main.py
@@ -1,51 +1,123 @@
 __author__ = 'cagdascaglak'
 
 import os
-from colyzer import SITES
-from lxml import etree
 import copy
 
+from lxml import etree
+
+from colyzer import SITES
+from colyzer.Document import RealShingles, VirtualShingles, Document
+
+
 real_paths = []
+virtual_hor = []
 
 
-def find_real_paths(tag, tmp_list):
+def find_real_paths(tag, tmp_list, d):
     tmp_list.append(tag.tag)
     if len(tag) != 0:
         for t in tag:
-            find_real_paths(t, tmp_list)
+            find_real_paths(t, tmp_list, d)
     new_list = copy.copy(tmp_list)
-    real_paths.append(new_list)
+    if len(new_list) > 2:
+        for i in range(0, len(new_list) - 2):
+            new_shingle_list = []
+            new_shingle_list.append(new_list[i])
+            new_shingle_list.append(new_list[i + 1])
+            new_shingle_list.append(new_list[i + 2])
+            real_shingles = RealShingles(new_shingle_list, d)
+            real_paths.append(real_shingles)
     if len(tmp_list) > 0:
         del tmp_list[len(tmp_list) - 1]
     return
 
 
-def find_all_real_paths(tag):
+def find_all_real_paths(tag, depth):
+    depth += 1
     for t in tag:
         temp_list = []
-        find_real_paths(t, temp_list)
-        find_all_real_paths(t)
+        find_real_paths(t, temp_list, depth)
+        find_all_real_paths(t, depth)
+
+
+def find_all_hor_paths(l, depth):
+    temp_list = []
+    temp_list_tag = []
+    for t in l:
+        temp_list.append(t.tag)
+        temp_list_tag.append(t)
+    if len(temp_list) is not 0:
+        new_list = copy.copy(temp_list)
+        virtual_shingles = VirtualShingles(new_list, depth)
+        virtual_hor.append(virtual_shingles)
+        depth += 1
+        for i in temp_list_tag:
+            find_all_hor_paths(i, depth)
+
+
+def calculate_distance(doci, docj):
+    intersection_sum0 = 0.0
+    diff_sum0 = 0.0
+    control = 0
+
+    for j in docj.real:
+        for i in doci.real:
+            if j.real_paths == i.real_paths:
+                control = 1
+                intersection_sum0 += abs(j.weight - i.weight)
+                print(j.weight)
+                print(i.weight)
+        if control == 0:
+            diff_sum0 += j.weight
+        control = 0
+
+
+    control = 0
+    intersection_sum1 = 0.0
+    diff_sum1 = 0.0
+    for j in docj.virtual:
+        for i in doci.virtual:
+            if j.virtual_paths == i.virtual_paths:
+                control = 1
+                intersection_sum1 += abs(j.weight - i.weight)
+        if control == 0:
+            diff_sum1 += j.weight
+        control = 0
+    return diff_sum0 + intersection_sum0 + diff_sum1 + intersection_sum1
+
 
 if __name__ == '__main__':
-
+    doc_list = []
     html_files_path = []
     for top_dir0, sub_dirs0, files0 in os.walk(os.path.join(SITES)):
         for site_dir in sub_dirs0:
             for top_dir1, sub_dirs1, page_files in os.walk(os.path.join(top_dir0, site_dir)):
                 for html_file in page_files:
-                    """
-                    with open(os.path.join(top_dir1, html_file)) as f:
-                        html = f.read()
-                    print(html)
-                    break
-                    """
-                    html_files_path.append(os.path.join(top_dir1, html_file))
 
-    tree = etree.parse("/Users/cagdascaglak/Desktop/default2.html", parser=etree.HTMLParser(remove_comments=True))
-    # print(tree.getroot().tag)
+                    print(os.path.join(top_dir1, html_file))
+                    tree = etree.parse(os.path.join(top_dir1, html_file),
+                                       parser=etree.HTMLParser(remove_comments=True))
 
-    root_tag = tree.getroot()[1]
+                    try:
+                        root_tag = tree.getroot()[1]
+                        find_all_real_paths(root_tag, -1)
+                        find_all_hor_paths(root_tag, 0)
 
-    find_all_real_paths(root_tag)
-    for i in real_paths:
-        print(i)
+                        new_real = copy.copy(real_paths)
+                        new_virt = copy.copy(virtual_hor)
+
+                        doc = Document(new_real, new_virt)
+                        doc_list.append(doc)
+
+                        del real_paths[:]
+                        del virtual_hor[:]
+
+                        html_files_path.append(os.path.join(top_dir1, html_file))
+                    except Exception:
+                        print(html_file)
+
+    distance_matrix = []
+
+
+    x = calculate_distance(doc_list[0], doc_list[0])
+    print(x)
