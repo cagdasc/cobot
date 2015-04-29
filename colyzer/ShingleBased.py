7b85a87e03d7cc7ffec62b820b30e87675ce25f5
Cagdas <cagdascaglak@gmail.com>
2015-04-29 14:20:36 +0300

    ShingleBased class added

---
+++ colyzer/ShingleBased.py
@@ -0,0 +1,82 @@
+__author__ = 'cagdas'
+
+from colyzer import tag_weight
+import copy
+
+
+class Document:
+
+    def __init__(self):
+        self.all_paths = []
+
+    def find_real_paths(self,tag, tmp_list, d):
+        tmp_list.append(tag.tag)
+        if len(tag) != 0:
+            for t in tag:
+                self.find_real_paths(t, tmp_list, d)
+        new_list = copy.copy(tmp_list)
+        shingles0 = Shingles(new_list, d, 0)
+        control = True
+        i = 0
+
+        while control and i < len(self.all_paths):
+            if shingles0.path == self.all_paths[i].path:
+                self.all_paths[i].weight += shingles0.weight
+                control = False
+            i += 1
+        if control:
+            self.all_paths.append(shingles0)
+
+        if len(tmp_list) > 0:
+            del tmp_list[len(tmp_list) - 1]
+        return
+
+    def find_all_real_paths(self, tag, depth):
+        depth += 1
+        for t in tag:
+            temp_list = []
+            self.find_real_paths(t, temp_list, depth)
+            self.find_all_real_paths(t, depth)
+
+    def find_all_hor_paths(self, l, depth):
+        temp_list = []
+        temp_list_tag = []
+        for t in l:
+            temp_list.append(t.tag)
+            temp_list_tag.append(t)
+        if len(temp_list) is not 0:
+            new_list = copy.copy(temp_list)
+            shingles0 = Shingles(new_list, depth, 1)
+            control = True
+            i = 0
+            while control and i < len(self.all_paths):
+                if shingles0.path == self.all_paths[i].path:
+                    self.all_paths[i].weight += shingles0.weight
+                    control = False
+                i += 1
+            if control:
+                self.all_paths.append(shingles0)
+
+            depth += 1
+            for i in temp_list_tag:
+                self.find_all_hor_paths(i, depth)
+
+
+class Shingles:
+    def __init__(self, path, depth, path_type):
+        self.path = path
+        self.depth = depth
+        w2 = 1.0
+        a = 10.0
+        w = 0.0
+        for i in range(0, len(self.path)):
+            try:
+                w1 = tag_weight[self.path[i]]
+            except Exception:
+                w1 = 0.1
+            if path_type == 0:
+                w3 = (a / ((i + self.depth) + a))
+            else:
+                w3 = (a / (self.depth + a))
+            w += ((w1 * w2 * w3) / len(self.path))
+        self.weight = w
--- colyzer/main.py
+++ colyzer/main.py
@@ -6,11 +6,9 @@ import copy
 from lxml import etree
 
 from colyzer import SITES
-from colyzer.Document import RealShingles, VirtualShingles, Document
+import ShingleBased
 
-
-real_paths = []
-virtual_hor = []
+all_paths = []
 
 
 def find_real_paths(tag, tmp_list, d):
@@ -19,11 +17,18 @@ def find_real_paths(tag, tmp_list, d):
         for t in tag:
             find_real_paths(t, tmp_list, d)
     new_list = copy.copy(tmp_list)
-    if len(new_list) > 2:
-        for i in range(0, len(new_list) - 2):
-            new_shingle_list = [new_list[i], new_list[i + 1], new_list[i + 2]]
-            real_shingles = RealShingles(new_shingle_list, d)
-            real_paths.append(real_shingles)
+    shingles0 = ShingleBased.Shingles(new_list, d, 0)
+    cntrl = True
+    i = 0
+
+    while cntrl and i < len(all_paths):
+        if shingles0.path == all_paths[i].path:
+            all_paths[i].weight += shingles0.weight
+            cntrl = False
+        i += 1
+    if cntrl:
+        all_paths.append(shingles0)
+
     if len(tmp_list) > 0:
         del tmp_list[len(tmp_list) - 1]
     return
@@ -45,8 +50,17 @@ def find_all_hor_paths(l, depth):
         temp_list_tag.append(t)
     if len(temp_list) is not 0:
         new_list = copy.copy(temp_list)
-        virtual_shingles = VirtualShingles(new_list, depth)
-        virtual_hor.append(virtual_shingles)
+        shingles0 = ShingleBased.Shingles(new_list, depth, 1)
+        cntrl = True
+        i = 0
+        while cntrl and i < len(all_paths):
+            if shingles0.path == all_paths[i].path:
+                all_paths[i].weight += shingles0.weight
+                cntrl = False
+            i += 1
+        if cntrl:
+            all_paths.append(shingles0)
+
         depth += 1
         for i in temp_list_tag:
             find_all_hor_paths(i, depth)
@@ -57,28 +71,15 @@ def calculate_distance(doci, docj):
     diff_sum0 = 0.0
     control = 0
 
-    for j in docj.real:
-        for i in doci.real:
-            if j.real_paths == i.real_paths:
+    for j in docj.all_paths:
+        for i in doci.all_paths:
+            if j.path == i.path:
                 control = 1
                 intersection_sum0 += abs(j.weight - i.weight)
         if control == 0:
             diff_sum0 += j.weight
         control = 0
-
-
-    control = 0
-    intersection_sum1 = 0.0
-    diff_sum1 = 0.0
-    for j in docj.virtual:
-        for i in doci.virtual:
-            if j.virtual_paths == i.virtual_paths:
-                control = 1
-                intersection_sum1 += abs(j.weight - i.weight)
-        if control == 0:
-            diff_sum1 += j.weight
-        control = 0
-    return diff_sum0 + intersection_sum0 + diff_sum1 + intersection_sum1
+    return diff_sum0 + intersection_sum0
 
 
 if __name__ == '__main__':
@@ -88,37 +89,49 @@ if __name__ == '__main__':
         for site_dir in sub_dirs0:
             for top_dir1, sub_dirs1, page_files in os.walk(os.path.join(top_dir0, site_dir)):
                 for html_file in page_files:
-
                     print(os.path.join(top_dir1, html_file))
                     tree = etree.parse(os.path.join(top_dir1, html_file),
                                        parser=etree.HTMLParser(remove_comments=True))
 
-                    try:
-                        root_tag = tree.getroot()[1]
-                        find_all_real_paths(root_tag, -1)
-                        find_all_hor_paths(root_tag, 0)
+                    # try:
+                    root_tag = tree.getroot()[1]
+                    doc = ShingleBased.Document()
+                    doc.find_all_real_paths(root_tag, -1)
+                    doc.find_all_hor_paths(root_tag, 0)
+                    doc_list.append(doc)
 
-                        new_real = copy.copy(real_paths)
-                        new_virtual = copy.copy(virtual_hor)
 
-                        doc = Document(new_real, new_virtual)
-                        doc_list.append(doc)
+                    """
+                    find_all_real_paths(root_tag, -1)
+                    find_all_hor_paths(root_tag, 0)
 
-                        del real_paths[:]
-                        del virtual_hor[:]
+                    new_all_paths = copy.copy(all_paths)
 
-                        html_files_path.append(os.path.join(top_dir1, html_file))
-                    except Exception:
-                        print(html_file)
+                    doc = ShingleBased.Document(new_all_paths)
+                    doc_list.append(doc)
+
+                    del all_paths[:]
+                    """
+
+                    html_files_path.append(os.path.join(top_dir1, html_file))
+                    #except Exception:
+                    #   print(html_file + ' Exp')
+
 
     distance_matrix = []
 
+    i = 0
+    j = 0
     for doci in doc_list:
         temp_dist = []
         for docj in doc_list:
             dist = calculate_distance(doci, docj)
             temp_dist.append(dist)
+            print str(i) + ' -- ' + str(j)
+            j +=1
         distance_matrix.append(temp_dist)
+        j = 0
+        i += 1
 
     for a in distance_matrix:
         print(a)
