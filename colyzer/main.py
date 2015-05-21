ae9b08fe8c14b768a72f2fa9cfa0c17203684995
Cagdas <cagdascaglak@gmail.com>
2015-05-21 23:40:03 +0300

    added debug mode

--- colyzer/main.py
+++ colyzer/main.py
@@ -1,5 +1,10 @@
 __author__ = 'cagdascaglak'
 
+import os
+from lxml import etree
+from colyzer import ShingleBased, SelkowTED, ClusterAlgorithms
+from colyzer import SITES
+
 import sys
 from twisted.internet import reactor
 from scrapy.crawler import Crawler
@@ -8,13 +13,50 @@ from cobot.spiders.crawler import PageWalker
 from scrapy.utils.project import get_project_settings
 
 if __name__ == '__main__':
-    spider = PageWalker(cfg=sys.argv[1])
-    settings = get_project_settings()
-    crawler = Crawler(settings)
-    crawler.signals.connect(reactor.stop, signal=signals.spider_closed)
-    crawler.configure()
-    crawler.crawl(spider)
-    crawler.start()
-    log.start(loglevel=log.INFO)
-    log.start_from_crawler(crawler)
-    reactor.run()
+    debug_mode = 1
+    if debug_mode == 0:
+        spider = PageWalker(cfg=sys.argv[1])
+        settings = get_project_settings()
+        crawler = Crawler(settings)
+        crawler.signals.connect(reactor.stop, signal=signals.spider_closed)
+        crawler.configure()
+        crawler.crawl(spider)
+        crawler.start()
+        log.start_from_crawler(crawler)
+        reactor.run()
+    else:
+        algorithm = 0  # 0 - shingle, 1 - ted
+
+        doc_list = []
+        html_files_path = []
+        for top_dir0, sub_dirs0, files0 in os.walk(os.path.join(SITES)):
+            for site_dir in sub_dirs0:
+                for top_dir1, sub_dirs1, page_files in os.walk(os.path.join(top_dir0, site_dir)):
+                    for html_file in page_files:
+                        print(os.path.join(top_dir1, html_file))
+                        tree = etree.parse(os.path.join(top_dir1, html_file),
+                                           parser=etree.HTMLParser(remove_comments=True))
+
+                        root_tag = tree.getroot()[1]
+
+                        if algorithm == 0:
+                            doc = ShingleBased.Document(html_file)
+                            doc.find_all_real_paths(root_tag, -1)
+                            doc.find_all_virtual_paths(root_tag, 0)
+                            doc_list.append(doc)
+                        else:
+                            doc = SelkowTED.Nodes(root_tag, doc_name=html_file)
+                            doc_list.append(doc)
+
+        if algorithm == 0:
+            distance_matrix = ShingleBased.get_distance_matrix(doc_list)
+            print(distance_matrix)
+            clustering = ClusterAlgorithms.Clustering(0.755, 500, 5)
+            clustering.process(doc_list, distance_matrix)
+            clustering.pretty_print()
+        else:
+            distance_matrix = SelkowTED.get_distance_matrix(doc_list)
+            print(distance_matrix)
+            clustering = ClusterAlgorithms.Clustering(0.755, 500, 5)
+            clustering.process(doc_list, distance_matrix)
+            clustering.pretty_print()
