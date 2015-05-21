ae6bf200661f44da769648ecbcb8f4103467a3be
Cagdas <cagdascaglak@gmail.com>
2015-05-21 21:15:04 +0300

    Config system added

--- cobot/config.py
+++ cobot/config.py
@@ -2,9 +2,10 @@ __author__ = 'cagdas'
 
 
 class Config:
-    def __init__(self, initialize, cobotsettings):
+    def __init__(self, initialize, cobotsettings, algorithm):
         self.initialize = initialize
         self.cobotsettings = cobotsettings
+        self.algorithm = algorithm
 
 
 class Initialize:
@@ -18,3 +19,24 @@ class CobotSettings:
     def __init__(self, robots, page_count):
         self.robots = robots  # obey the robots.txt boolean
         self.page_count = page_count  # page count
+
+
+class Algorithm:
+    def __init__(self, shingle, ted, which):
+        self.which = which
+        self.shingle = shingle
+        self.ted = ted
+
+
+class ShingleAlgorithm:
+    def __init__(self, cluster_size, threshold, iteration):
+        self.cluster_size = cluster_size
+        self.threshold = threshold
+        self.iteration = iteration
+
+
+class TEDAlgorithm:
+    def __init__(self, cluster_size, threshold, iteration):
+        self.cluster_size = cluster_size
+        self.threshold = threshold
+        self.iteration = iteration
