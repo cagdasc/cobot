5ea0c54322bf4c2d916dbf4ab99040f2b1cf37eb
Cagdas <cagdascaglak@gmail.com>
2015-05-25 14:11:03 +0300

    save raw link and name

--- cobot/pipelines.py
+++ cobot/pipelines.py
@@ -7,43 +7,62 @@
 
 from lxml import etree
 from colyzer import ShingleBased, SelkowTED, ClusterAlgorithms
-
+import json
+import os
+from cobot.spiders import SITES
+from cobot import RawDocuments
 
 class CobotPipeline(object):
     def __init__(self):
         self.doc_list = []
-        self.ted_dist = []
 
     def open_spider(self, spider):
-        print('OPENED!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!111111')
+        print('SPIDER OPENED!!')
         print('Algorithm -->>' + spider.algorithm.which)
 
     def process_item(self, item, spider):
         print('page name -->' + item['page_name'])
-        print('Processing....')
         self.__create_doc_list(item, spider.algorithm.which)
         return item
 
     def close_spider(self, spider):
-        print('CLOSED!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!1111111111')
+        print('SPIDER CLOSED!!')
+        raw_doc_list = []
+        for doc in self.doc_list:
+            raw_doc = ClusterAlgorithms.Document(doc.doc_name, doc.doc_link)
+            raw_doc_list.append(raw_doc.__dict__)
+        result = RawDocuments(raw_doc_list)
+
+        json_txt = json.dumps(result.__dict__)
+        with open(os.path.join(SITES, spider.initialize.site_name + '_raw.json'), 'w') as f:
+            f.write(json_txt)
+
         if spider.algorithm.which == 'shingle':
             distance_matrix = ShingleBased.get_distance_matrix(self.doc_list)
             print(distance_matrix)
-            # clustering = ClusterAlgorithms.Clustering(0.755, 500, 5)
-            clustering = ClusterAlgorithms.Clustering(spider.algorithm.shingle.threshold,
-                                                      spider.algorithm.shingle.iteration,
-                                                      spider.algorithm.shingle.cluster_size,
-                                                      spider.initialize.site_name)
-            clustering.process(self.doc_list, distance_matrix)
-            clustering.pretty_print()
         elif spider.algorithm.which == 'ted':
             distance_matrix = SelkowTED.get_distance_matrix(self.doc_list)
             print(distance_matrix)
-            # clustering = ClusterAlgorithms.Clustering(0.755, 500, 5)
-            clustering = ClusterAlgorithms.Clustering(spider.algorithm.ted.threshold, spider.algorithm.ted.iteration,
-                                                      spider.algorithm.ted.cluster_size, spider.initialize.site_name)
+        else:
+            print('There is no algorithm!!')
+            return
+
+        if spider.algorithm.which_clustering == 'k_means':
+            clustering = ClusterAlgorithms.Clustering(None,
+                                                      spider.algorithm.k_means.iteration,
+                                                      spider.algorithm.k_means.cluster_size,
+                                                      spider.initialize.site_name)
+            clustering.k_means_process(self.doc_list, distance_matrix)
+        elif spider.algorithm.which_clustering == 'shingle_based':
+            clustering = ClusterAlgorithms.Clustering(spider.algorithm.shingle_based.threshold,
+                                                      spider.algorithm.shingle_based.iteration,
+                                                      spider.algorithm.shingle_based.cluster_size,
+                                                      spider.initialize.site_name)
             clustering.process(self.doc_list, distance_matrix)
-            clustering.pretty_print()
+        else:
+            print('There is no clustering algorithm!!')
+            return
+        clustering.pretty_print()
 
     def __create_doc_list(self, item, which):
         tree = etree.parse(item['page_full_path'], parser=etree.HTMLParser(remove_comments=True))
