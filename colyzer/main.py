0d3bdbde75f145cb3fc43adfdaccc76ec4139555
Cagdas <cagdascaglak@gmail.com>
2015-05-29 12:10:17 +0300

    offline and online analyzing

--- colyzer/main.py
+++ colyzer/main.py
@@ -15,7 +15,6 @@
 __author__ = 'cagdascaglak'
 
 import os
-from lxml import etree
 from colyzer import ShingleBased, SelkowTED, ClusterAlgorithms
 from cobot.spiders import SITES
 import json
@@ -28,12 +27,32 @@ from scrapy import log, signals
 from cobot.spiders.crawler import PageWalker
 from scrapy.utils.project import get_project_settings
 
+from cobot.pipelines import get_all_form, create_tree
+from lxml import etree
+import time
+
+def print_node(root):
+    for r in root:
+        print(r.tag)
+        print_node(r)
+
 if __name__ == '__main__':
 
+    start = time.time()
     with open(sys.argv[1]) as f:
         config_file = f.read()
         print(config_file)
         __config = json.loads(config_file, object_hook=lambda d: namedtuple('X', d.keys())(*d.values()))
+
+    cluster_size = 0
+    if __config.algorithm.which_clustering == 'k_means':
+        cluster_size = __config.algorithm.k_means.cluster_size
+    elif __config.algorithm.which_clustering == 'shingle_based':
+        cluster_size = __config.algorithm.shingle_based.cluster_size
+
+    if __config.cobot_settings.page_count < cluster_size:
+        print('Number of clusters must be less than number of pages!!')
+        sys.exit(status=1)
 
     if __config.crawling:
         spider = PageWalker(__config)
@@ -47,31 +66,8 @@ if __name__ == '__main__':
         reactor.run()
     else:
         algorithm = __config.algorithm.which
-
         doc_list = []
-        """
-        html_files_path = []
-        for top_dir0, sub_dirs0, files0 in os.walk(os.path.join(SITES)):
-            for site_dir in sub_dirs0:
-                for top_dir1, sub_dirs1, page_files in os.walk(os.path.join(top_dir0, site_dir)):
-                    for html_file in page_files:
-                        print(os.path.join(top_dir1, html_file))
-                        tree = etree.parse(os.path.join(top_dir1, html_file),
-                                           parser=etree.HTMLParser(remove_comments=True))
 
-                        root_tag = tree.getroot()[1]
-
-                        if algorithm == 'shingle':
-                            doc = ShingleBased.Document(html_file)
-                            doc.find_all_real_paths(root_tag, -1)
-                            doc.find_all_virtual_paths(root_tag, 0)
-                            doc_list.append(doc)
-                        elif algorithm == 'ted':
-                            doc = SelkowTED.Nodes(root_tag, doc_name=html_file)
-                            doc_list.append(doc)
-                        else:
-                            print('There is no algorithm!!')
-        """
         print(os.path.join(SITES, __config.initialize.site_name))
 
         with open(os.path.join(SITES, __config.initialize.site_name + '_raw.json')) as f:
@@ -94,16 +90,22 @@ if __name__ == '__main__':
                 root_tag = tree.getroot()[1]
 
                 if algorithm == 'shingle':
-                    doc = ShingleBased.Document(html_file, doc_link=doc_link)
+                    doc = ShingleBased.ShingleDocument(html_file, doc_link=doc_link)
                     doc.find_all_real_paths(root_tag, -1)
                     doc.find_all_virtual_paths(root_tag, 0)
                     doc_list.append(doc)
                 elif algorithm == 'ted':
-                    doc = SelkowTED.Nodes(root_tag, doc_name=html_file, doc_link=doc_link)
+                    form_list = []
+                    get_all_form(root_tag, form_list)
+                    temp_root = etree.Element('root')
+                    create_tree(temp_root, form_list)
+
+                    doc = SelkowTED.Nodes(temp_root, doc_name=html_file, doc_link=doc_link)
                     doc_list.append(doc)
+
                 else:
                     print('There is no algorithm!!')
-
+        distance_matrix = []
         if algorithm == 'shingle':
             distance_matrix = ShingleBased.get_distance_matrix(doc_list)
             print(distance_matrix)
@@ -124,9 +126,10 @@ if __name__ == '__main__':
                                                       __config.algorithm.shingle_based.iteration,
                                                       __config.algorithm.shingle_based.cluster_size,
                                                       __config.initialize.site_name)
-            # clustering.process(doc_list, distance_matrix)
-            clustering.process(doc_list, distance_matrix)
+            clustering.shingle_based_process(doc_list, distance_matrix)
         else:
             print('There is no clustering algorithm!!')
         clustering.pretty_print()
 
+    end = time.time()
+    print(end - start)
